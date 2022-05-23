4070491ec0f7350c6146b13cb1f5a4be94926d07
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-23 13:40:31 -0400

    updating intro

--- install-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install chromium and required fonts
-# available fonts https://wiki.alpinelinux.org/wiki/Fonts
-apk add chromium font-noto-cjk font-noto-emoji \
-    terminus-font ttf-dejavu ttf-freefont ttf-font-awesome \
-    ttf-inconsolata ttf-linux-libertine \
-    && fc-cache -f
